aac717306bb316b1e2adfea0ec9d907813dae2db
Mamn847 <133536689+Mamn847@users.noreply.github.com>
2024-05-02 08:06:23 +0300

    Create Base repo with crud

---
+++ DiscussionForum/Repositories/BaseRepository.cs
@@ -0,0 +1,41 @@
+﻿
+using DiscussionForum.Models;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscussionForum.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        private readonly ForumContext _forumContext;
+        public BaseRepository(ForumContext forumContext)
+        {
+            _forumContext = forumContext;
+        }
+        public async Task<T> AddAsync(T entity)
+        {
+            await _forumContext.Set<T>().AddAsync(entity);
+            await _forumContext.SaveChangesAsync();
+            return entity;
+        }
+        public void Delete(T entity)
+        {
+            _forumContext.Set<T>().Remove(entity);
+            _forumContext.SaveChanges();
+        }
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+           return await  _forumContext.Set<T>().ToListAsync();
+        }
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _forumContext.Set<T>().FindAsync(id);
+        }
+        public async Task<T> Update(T entity)
+        {
+            _forumContext.Update(entity);
+            await _forumContext.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
