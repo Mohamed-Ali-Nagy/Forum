c045c1b2a944ad8932714fd2193b810ea4c9a564
Mamn847 <133536689+Mamn847@users.noreply.github.com>
2024-05-02 08:48:56 +0300

    add unit of work

---
+++ DiscussionForum/Repositories/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using DiscussionForum.Models;
+
+namespace DiscussionForum.Repositories
+{
+    public interface IUnitOfWork:IDisposable
+    {
+        public IBaseRepository<Answer> Answers { get;}
+        public IBaseRepository<Question> Questions { get;}
+        public IBaseRepository<Like> Likes { get;}
+        public IBaseRepository<Report> Reports  { get;}
+        int Complete();
+    }
+}
---
+++ DiscussionForum/Repositories/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using DiscussionForum.Models;
+
+namespace DiscussionForum.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+
+        private readonly ForumContext _forumContext;
+        public IBaseRepository<Answer> Answers {  get;private set; }
+
+        public IBaseRepository<Question> Questions {  get; private set; }
+
+        public IBaseRepository<Like> Likes {  get; private set; }
+
+        public IBaseRepository<Report> Reports { get; private set; }
+        public UnitOfWork(ForumContext forumContext)
+        {
+
+            _forumContext = forumContext;
+            Answers = new BaseRepository<Answer>(_forumContext);
+            Questions = new BaseRepository<Question>(_forumContext);
+            Likes = new BaseRepository<Like>(_forumContext);
+            Reports = new BaseRepository<Report>(_forumContext);
+
+        }
+        public  int Complete()
+        {
+            return  _forumContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _forumContext.Dispose();
+        }
+    }
+}
