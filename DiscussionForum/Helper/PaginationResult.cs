91715e2a1d0c3b9d217d2569c74ae41ad6b65f35
Mamn847 <133536689+Mamn847@users.noreply.github.com>
2024-05-03 17:58:12 +0300

    add pagination result class

---
+++ DiscussionForum/Helper/PaginationResult.cs
@@ -0,0 +1,16 @@
+﻿namespace DiscussionForum.Helper
+{
+    public class PaginationResult<T>
+    {
+        public IEnumerable<T> List { get; set; }
+        public int PageSize { get; set; }
+        public int PageIndex { get; set; }
+
+        public PaginationResult(IEnumerable<T>list,int size,int index)
+        {
+            List = list;
+            PageSize = size;
+            PageIndex = index;
+        }
+    }
+}
